88b08ded8efd4bc0fe46100755c0b3d5c8b756c3
KaanAtbas <kaanerc@gmail.com>
2024-02-16 15:46:05 +0300

    all changes

--- modules/pumps/besGiris.ino
+++ modules/pumps/besGiris.ino
@@ -1,10 +1,10 @@
 //besiyeri giriş
 
-#define POMPA_PIN 7 // Peristaltik pompa için kullanılan pin
+#define POMPA_BESIYERI 7 // Peristaltik pompa için kullanılan pin
 #define SIVI_SEVIYE_SENSORU_PIN 2 // Dijital sıvı seviye sensörü için kullanılan pin
 
 void setup() {
-  pinMode(POMPA_PIN, OUTPUT);
+  pinMode(POMPA_BESIYERI, OUTPUT);
   pinMode(SIVI_SEVIYE_SENSORU_PIN, INPUT);
 }
 
@@ -14,9 +14,9 @@ void pompa1Calistir() {
 
   // Eğer sıvı seviyesi 0'a eşitse, pompayı çalıştır
   while (siviSeviyesi = 0) {
-    digitalWrite(POMPA_PIN, HIGH); // Pompa çalıştır
+    digitalWrite(POMPA_BESIYERI, HIGH); // Pompa çalıştır
     siviSeviyesi = digitalRead(SIVI_SEVIYE_SENSORU_PIN); // Sıvı seviyesini tekrar oku
   }
 
-  digitalWrite(POMPA_PIN, LOW); // Pompa durdur
+  digitalWrite(POMPA_BESIYERI, LOW); // Pompa durdur
 }
