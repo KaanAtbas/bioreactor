7d452b88377c0afc22e7222ea98df3e81e712736
KaanAtbas <kaanerc@gmail.com>
2024-02-16 15:41:09 +0300

    V2

--- modules/pumps/pump2tpH.ino
+++ modules/pumps/pump2tpH.ino
@@ -5,11 +5,11 @@
 
 
 // Pin tanımlamaları
-#define POMPA_PIN 9  // Peristaltik pompa için kullanılan pin
+#define POMPA_TAHLIYE 9  // Peristaltik pompa için kullanılan pin
 #define SEVIYE_SENSORU_PIN 7  // Dijital sıvı seviye sensörü için kullanılan pin
 
 void setup() {
-  pinMode(POMPA_PIN, OUTPUT);  // Pompa pinini çıkış olarak ayarla
+  pinMode(POMPA_TAHLIYE, OUTPUT);  // Pompa pinini çıkış olarak ayarla
   pinMode(SEVIYE_SENSORU_PIN, INPUT);  // Sıvı seviye sensörü pinini giriş olarak ayarla
 }
 
@@ -18,11 +18,11 @@ void loop() {
   int seviye = digitalRead(SEVIYE_SENSORU_PIN);
   
   // Eğer sıvı seviyesi 0 ise pompayı çalıştır
-  if (seviye == 0) {
-    digitalWrite(POMPA_PIN, HIGH);  // Pompa çalıştır
+while (POMPA_BAZ == HIGH) {
+    digitalWrite(POMPA_TAHLIYE, HIGH);  // Pompa çalıştır
   } else {
   
-    digitalWrite(POMPA_PIN, LOW);  // Pompa durdur
+    digitalWrite(POMPA_TAHLIYE, LOW);  // Pompa durdur
   }
 }
 
