52408255d45cd9ecbe61a1ec9c69327a0ecb80a0
KaanAtbas <kaanerc@gmail.com>
2024-02-16 15:49:37 +0300

    SON

--- modules/pumps/filtresizTahliye.ino
+++ modules/pumps/filtresizTahliye.ino
@@ -1,11 +1,11 @@
 //filtresiz tahliye
 
 // Pin tanımlamaları
-#define pompaPin = 2; // Pompanın kontrol pin numarası
+#define POMPA_FILTRESIZ = 2; // Pompanın kontrol pin numarası
 #define butonPin = 3; // Butonun bağlı olduğu pin numarası
 
 void setup() {
-  pinMode(pompaPin, OUTPUT); // Pompa pinini çıkış olarak ayarla
+  pinMode(POMPA_FILTRESIZ, OUTPUT); // Pompa pinini çıkış olarak ayarla
   pinMode(butonPin, INPUT_PULLUP); // Buton pini giriş olarak ayarla ve dahili pull-up direncini etkinleştir
 }
 
@@ -15,9 +15,9 @@ void loop() {
 
   // Butona basıldığında (HIGH durumunda), pompayı çalıştır
   if (butonDurumu == HIGH) {
-    digitalWrite(pompaPin, HIGH); // Pompayı aç
+    digitalWrite(POMPA_FILTRESIZ, HIGH); // Pompayı aç
   } else {
-    digitalWrite(pompaPin, LOW); // Pompayı kapat
+    digitalWrite(POMPA_FILTRESIZ, LOW); // Pompayı kapat
   }
 }
 
