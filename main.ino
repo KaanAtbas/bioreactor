ca21e6f68297aa420706852abc3bfb14e357373d
iso53 <102249575+ISO53@users.noreply.github.com>
2024-02-17 18:08:41 +0300

    Update main.ino

--- main.ino
+++ main.ino
@@ -2,16 +2,26 @@
 #include "pumps.ino"
 #include "heat.ino"
 
+#define besiyeri_pompası= 8; //besiyeri
+#define baz_pompası = 2; //baz
+#define tahliye_pompası= 3; //tahliye
+#define ldkfsş = 4; //filtresiz tahliye
+
+// bu pompaları bir array de sakla ayrı bir sınıfta da olabilir. o array "pumps" olsun mesela 
+
 void setup() {
     initializeSensors();
-    initializePumps();
+    initializePumps(pumps); // pumpsı buraya ver
     initializeHeat();
 }
 
 void loop() {
-    while (heatData<)
+    while (true)
     {
-        /* code */
+        // if (heat is low) 
+            manage_pump(ısı_pompası, ON) 
+        //else
+            manage_pump(ısı_pompası, OFF) 
     }
     
 
